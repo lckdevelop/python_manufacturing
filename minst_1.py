89953abf44161165cb07397d1bebeca9301e1f07
chakyeong lee <2chaekyeong@gmail.com>
2020-09-26 15:36:57 +0900

    numbering

--- minst_1.py
+++ minst_1.py
@@ -1,27 +1,30 @@
+# minst_1파일은 머신을 학습 시키는 파일
 import tensorflow as tf
+# MNIST데이터 가져오기
+from tensorflow.examples.tutorials.mnist import input_data
 
-#minst_1파일은 머신을 학습 시키는 파일
-
-#입력
+# 입력
 # 해당 경로에 데이터 생성
 # images파일에는 배열, labels파일에는 결과가 들어있다.
-from tensorflow.examples.tutorials.mnist import input_data
+
+# mnist의 데이터를 읽어 들이는 메서드 /tmp/data : 데이터가 저장될 위치 , 데이터에 레이블을 지정하는 방식
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 # 모델
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.placeholder(tf.float32, [None, 784]) # placeholder: 연산 그래프가 실행될 때 제공되어야 하는 값
 
-W = tf.Variable(tf.zeros([784, 10]))
+W = tf.Variable(tf.zeros([784, 10])) # variable: 연산 과정에서 조작되는 값
 b = tf.Variable(tf.zeros([10]))
 
-y_model = tf.matmul(x, W) + b
+y_model = tf.matmul(x, W) + b # 예측 모델
 
 # 라벨
-y = tf.placeholder(tf.float32, [None, 10])
+y = tf.placeholder(tf.float32, [None, 10]) # 정답 라벨
 
-# loss와 optimizer
+# 손실함수(loss)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=y_model))
-optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cost) # learning_rate = 0.01
+# 손실 함수의 값을 최소화 하는 방법
+optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cost)  # learning_rate = 0.01
 
 # 세션
 sess = tf.Session()
@@ -37,7 +40,7 @@ for epoch in range(25):
         _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
         avg_cost += c / total_batch
 
-    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+    print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
 
 print("최적화 완료")
 
--- minst_2.py
+++ minst_2.py
@@ -6,18 +6,18 @@ from scipy import ndimage
 
 # 판단할 데이터를 실행하는 데이터
 def getBestShift(img):
-    cy,cx = ndimage.measurements.center_of_mass(img)
+    cy, cx = ndimage.measurements.center_of_mass(img)
 
-    rows,cols = img.shape
-    shiftx = np.round(cols/2.0-cx).astype(int)
-    shifty = np.round(rows/2.0-cy).astype(int)
+    rows, cols = img.shape
+    shiftx = np.round(cols / 2.0 - cx).astype(int)
+    shifty = np.round(rows / 2.0 - cy).astype(int)
 
-    return shiftx,shifty
+    return shiftx, shifty
 
-def shift(img,sx,sy):
-    rows,cols = img.shape
-    M = np.float32([[1,0,sx],[0,1,sy]])
-    shifted = cv2.warpAffine(img,M,(cols,rows))
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
     return shifted
 
 # 모델
@@ -36,22 +36,20 @@ sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
 
-# 앞에서 학습한 것을 불러옴옴model_path = "/tmp/model.saved"
+# 앞에서 학습한 것을 불러옴 model_path = "/tmp/model.saved"
 model_path = "/tmp/model.saved"
 saver = tf.train.Saver()
-
 
 saver.restore(sess, model_path)
 print("Model restored from file: %s" % model_path)
 
 # 10개의 데이터를 받는다고 선언
 # 784개의 행렬로 이루어짐
-images = np.zeros((10,784))
+images = np.zeros((10, 784))
 
 i = 0
 for no in range(10):
-
-    gray = cv2.imread(str(no)+".png", 0)
+    gray = cv2.imread(str(no) + ".png", 0)
     gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
 
     (thresh, gray) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
@@ -59,47 +57,45 @@ for no in range(10):
     while np.sum(gray[0]) == 0:
         gray = gray[1:]
 
-    while np.sum(gray[:,0]) == 0:
-        gray = np.delete(gray,0,1)
+    while np.sum(gray[:, 0]) == 0:
+        gray = np.delete(gray, 0, 1)
 
     while np.sum(gray[-1]) == 0:
         gray = gray[:-1]
 
-    while np.sum(gray[:,-1]) == 0:
-        gray = np.delete(gray,-1,1)
+    while np.sum(gray[:, -1]) == 0:
+        gray = np.delete(gray, -1, 1)
 
-    rows,cols = gray.shape
+    rows, cols = gray.shape
 
     cv2.imwrite("b_" + str(no) + ".png", gray)
 
     if rows > cols:
-        factor = 20.0/rows
+        factor = 20.0 / rows
         rows = 20
-        cols = int(round(cols*factor))
+        cols = int(round(cols * factor))
         # first cols than rows
-        gray = cv2.resize(gray, (cols,rows))
+        gray = cv2.resize(gray, (cols, rows))
     else:
-        factor = 20.0/cols
+        factor = 20.0 / cols
         cols = 20
-        rows = int(round(rows*factor))
+        rows = int(round(rows * factor))
         # first cols than rows
         gray = cv2.resize(gray, (cols, rows))
 
-    colsPadding = (int(math.ceil((28-cols)/2.0)),int(math.floor((28-cols)/2.0)))
-    rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
-    gray = np.lib.pad(gray,(rowsPadding,colsPadding),'constant')
+    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
 
-    shiftx,shifty = getBestShift(gray)
-    shifted = shift(gray,shiftx,shifty)
+    shiftx, shifty = getBestShift(gray)
+    shifted = shift(gray, shiftx, shifty)
     gray = shifted
 
-
     cv2.imwrite("image_" + str(no) + ".png", gray)
-
 
     flatten = gray.flatten() / 255.0
     images[i] = flatten
 
     i += 1
 
-print (sess.run(tf.argmax(y_model,1), feed_dict={x: images}))
+print(sess.run(tf.argmax(y_model, 1), feed_dict={x: images}))
